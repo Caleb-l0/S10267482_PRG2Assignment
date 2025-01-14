e18b5763a5ae670a9957534bd3afbdcee18d1e12
Caleb <S10267482@connect.np.edu.sg>
2025-01-14 10:36:57 +0800

    Add project files.

---
+++ Basic_Features/Program.cs
@@ -0,0 +1,32 @@
+﻿class NORMFlight
+{
+    public double CalculateFees()
+    {
+
+    }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
+class LWTTFlight
+{
+    public double RequestFee { get; set; }
+
+
+    class Airline
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+
+        public Dictionary<string, Flight> flights = new Dictionary<string, Flight>();
+
+        public Airline(string name, string code)
+        {
+            this.name = name;
+            this.code = code;
+        }
+
+
+    }
